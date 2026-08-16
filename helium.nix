2734dbc86c93b7ef90b7b6fb58ede38b1c79df54
tekq <81379378+tekq@users.noreply.github.com>
2026-08-16 08:29:10 +0000

    chore: update Helium to latest

--- helium.nix
+++ helium.nix
@@ -71,7 +71,7 @@
 
 let
   pname = "helium";
-  version = "0.15.4.1";
+  version = "0.15.5.1";
 
   suffix = {
     aarch64-linux = "arm64";
@@ -80,7 +80,7 @@ let
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-bin_${version}-1_${suffix}.deb";
-    sha256 = "sha256-p10p9QFsZS0D/6oyPrSFV2UYqHE75BbOa6/bGzcE0Ag=";
+    sha256 = "sha256-jF5qPYeCibruFqqH+0lhn6n2df5K31xVbaU7xh9BjoM=";
   };
 
   inherit (lib) optional optionals makeLibraryPath makeSearchPathOutput makeBinPath;
